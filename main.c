1e27895ccd952188b1d22d98152d97918277d73e
ismannr <isyman.nurfauzan@binus.ac.id>
2026-04-19 23:36:54 +0700

    minimalize setup

--- main.c
+++ main.c
@@ -1,139 +1,79 @@
-/**
- * @file main
- *
- */
+//
+// Created by Isyman Nurfauzan Rustama on 4/19/2026.
+// Copyright (c) 2026 I-Tune Engineering. All rights reserved.
+//
 
-/*********************
- *      INCLUDES
- *********************/
 #include "lvgl/lvgl.h"
-#include "lvgl/examples/lv_examples.h"
-#include "lvgl/demos/lv_demos.h"
+#include "ui/ui.h"
+
+// =========================================================================
+// --- SIMULATOR ONLY START ---
+// =========================================================================
+
 #include "SDL_main.h"
-/*********************
- *      DEFINES
- *********************/
 
-/**********************
- *      TYPEDEFS
- **********************/
-
-/**********************
- *  STATIC PROTOTYPES
- **********************/
-static lv_display_t * hal_init(int32_t w, int32_t h);
-
-/**********************
- *  STATIC VARIABLES
- **********************/
-
-/**********************
- *      MACROS
- **********************/
-
-/**********************
- *   GLOBAL FUNCTIONS
- **********************/
-
-/*********************
- *      DEFINES
- *********************/
-
-/**********************
- *      TYPEDEFS
- **********************/
-
-/**********************
- *      VARIABLES
- **********************/
-
-/**********************
- *  STATIC PROTOTYPES
- **********************/
-
-/**********************
- *   GLOBAL FUNCTIONS
- **********************/
-
+/* Windows SDL2 Linker Bridge */
 #ifdef _WIN32
 #undef main
 int SDL_main(int argc, char *argv[]) {
- return main(argc, argv);
+    return main(argc, argv);
 }
 #endif
 
-int main(int argc, char **argv)
-{
-  /*Initialize LVGL*/
-  lv_init();
-
-  /*Initialize the display, and the input devices*/
-  hal_init(480, 320);
-
-  /*Open a demo or an example*/
-  if (argc <= 1) {
-	/*If not argument if provided just a open a demo or an example.*/
-    lv_demo_widgets();
-//      lv_example_chart_1();
-  } else {
-	/*Process the command line arguments and open the related demo*/
-    if (!lv_demos_create(&argv[1], argc - 1)) {
-      lv_demos_show_help();
-      goto demo_end;
-    }
-  }
-
-  /*To hide the memory and performance indicators in the corners
-   *disable `LV_USE_MEM_MONITOR` and `LV_USE_PERF_MONITOR` in `lv_conf.h`*/
-
-  while(1) {
-    /* Periodically call the lv_timer_handler. */
-    uint32_t time_until_next = lv_timer_handler();
-
-    if (time_until_next == LV_NO_TIMER_READY) {
-      /* Simply sleep for a while. */
-      time_until_next = LV_DEF_REFR_PERIOD;
-    }
-
-    lv_delay_ms(time_until_next);
-  }
-
-demo_end:
-  lv_deinit();
-  return 0;
-}
-
-/**********************
- *   STATIC FUNCTIONS
- **********************/
-
-/**
- * Initialize the Hardware Abstraction Layer (HAL) for the LVGL graphics
- * library
- */
+/* PC Simulator Display & Mouse Setup */
 static lv_display_t * hal_init(int32_t w, int32_t h)
 {
-  lv_group_set_default(lv_group_create());
+    lv_group_set_default(lv_group_create());
 
-  lv_display_t * disp = lv_sdl_window_create(w, h);
+    /* Create a desktop window */
+    lv_display_t * disp = lv_sdl_window_create(w, h);
 
-  lv_indev_t * mouse = lv_sdl_mouse_create();
-  lv_indev_set_group(mouse, lv_group_get_default());
-  lv_indev_set_display(mouse, disp);
-  lv_display_set_default(disp);
+    /* Create a simulated touch screen using the PC mouse */
+    lv_indev_t * mouse = lv_sdl_mouse_create();
+    lv_indev_set_group(mouse, lv_group_get_default());
+    lv_indev_set_display(mouse, disp);
+    lv_display_set_default(disp);
 
-  LV_IMAGE_DECLARE(mouse_cursor_icon); /*Declare the image file.*/
-  lv_obj_t * cursor_obj;
-  cursor_obj = lv_image_create(lv_screen_active()); /*Create an image object for the cursor */
-  lv_image_set_src(cursor_obj, &mouse_cursor_icon);           /*Set the image source*/
-  lv_indev_set_cursor(mouse, cursor_obj);             /*Connect the image  object to the driver*/
+    /* Set up the mouse cursor icon */
+    LV_IMAGE_DECLARE(mouse_cursor_icon);
+    lv_obj_t * cursor_obj = lv_image_create(lv_screen_active());
+    lv_image_set_src(cursor_obj, &mouse_cursor_icon);
+    lv_indev_set_cursor(mouse, cursor_obj);
 
-  lv_indev_t * mousewheel = lv_sdl_mousewheel_create();
-  lv_indev_set_display(mousewheel, disp);
+    return disp;
+}
+// =========================================================================
+// --- SIMULATOR ONLY END ---
+// =========================================================================
 
-  lv_indev_t * keyboard = lv_sdl_keyboard_create();
-  lv_indev_set_display(keyboard, disp);
-  lv_indev_set_group(keyboard, lv_group_get_default());
 
-  return disp;
+int main(int argc, char **argv)
+{
+    /* 1. Initialize LVGL core */
+    lv_init();
+
+    /* 2. Initialize the display and input devices */
+    // --- SIMULATOR ONLY! ---
+    hal_init(1024, 600);
+    // --- ON ESP32: call custom MIPI-DSI init function here ---
+
+    /* 3. Initialize your custom UI */
+    ui_init();
+
+    /* 4. The Main Loop */
+    while(1) {
+        /* Periodically call the lv_timer_handler to render graphics and read touches */
+        uint32_t time_until_next = lv_timer_handler();
+
+        if (time_until_next == LV_NO_TIMER_READY) {
+            time_until_next = LV_DEF_REFR_PERIOD; /* Default sleep time */
+        }
+
+        /* Sleep until the next frame needs to be drawn */
+        // --- SIMULATOR ONLY: Uses SDL delay ---
+        lv_delay_ms(time_until_next);
+        // --- ON ESP32: Replace this with vTaskDelay(pdMS_TO_TICKS(time_until_next)); ---
+    }
+
+    return 0;
 }
---
+++ ui/ui.h
@@ -0,0 +1,13 @@
+//
+// Created by Isyman Nurfauzan Rustama on 4/19/2026.
+// Copyright (c) 2026 I-Tune Engineering. All rights reserved.
+//
+
+
+#ifndef UI_H
+#define UI_H
+#include "lvgl/lvgl.h"
+
+void ui_init(void);
+
+#endif //UI_H
